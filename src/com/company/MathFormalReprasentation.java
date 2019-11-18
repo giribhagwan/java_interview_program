fb10cdfe97b329f4068e7e00c6f0104168885bc2
bhagwan.opallabs@gmail.com <Bhagwan@234>
2019-11-18 23:20:07 +0530

    pull request

--- src/com/company/MathFormalReprasentation.java
+++ src/com/company/MathFormalReprasentation.java
@@ -6,7 +6,29 @@ import java.util.Scanner;
 
 public class MathFormalReprasentation {
 
+    static int numberPower(int p){
+        int n=2,result=1;
+        if(n>=0&&p==0)
+        {
+            result=1;
+        }
+        else if(n==0&&p>=1)
+        {
+            result=0;
+        }
+        else
+        {
+            for(int i=1;i<=p;i++)
+            {
+                result=result*n;
+            }
+        }
+        return result;
+    }
     public static void main(String[] args) {
+
+
+
         Scanner in = new Scanner(System.in);
         int t=in.nextInt();
         int a = 0,b = 0,n = 0;
@@ -16,20 +38,21 @@ public class MathFormalReprasentation {
              b = in.nextInt();
              n = in.nextInt();
             int[] r= new int[n];
-            int count=0;
-             for(int j=1;j<n;j++){
-                 count++;
+             for(int j=0;j<n;j++){
                  int rr=0;
-               do{
-                    rr+=(j*j)+b;
-               }while (j>count);
+                 for(int l=0;l<=j;l++) {
+                     int re=0;
+                         re=numberPower(l)*b;
+                     rr +=re;
+                 }
                 r[j]=a+rr;
              }
              result.add(r);
         }
         result.forEach(e->{
-            for (int i=0;i<e.length;i++)
-                System.out.print("\t"+e[i]);
+            for (int i=0;i<e.length;i++) {
+                System.out.print(e[i]+" ");
+            }
             System.out.println();
         });
         in.close();
